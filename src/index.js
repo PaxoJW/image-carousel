84454ebe131cc093a39f3c3e0ef49ce3a1fc8c97
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2025-11-26 08:59:00 +0100

    Added images
    Added the images and re-organized the html structure.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './style.css'
+import './style.css';
+
+// 1. The function r (require.context(...)) gives r.keys() which are the images paths
+// and r(key) which gives the built asset URL
+function importAll(r) {
+    return r.keys().map(r); 
+}
+
+// 2. Create a require.context instance - a webpack function
+const images = importAll(
+  require.context('../assets', false, /\.(png|jpe?g|svg)$/)
+);
+
+// 3. Append to DOM
+const container = document.querySelector('.container');
+
+images.forEach(src => {
+  const img = document.createElement('img');
+  img.src = src; // Webpack replaces this with the final file URL
+  container.appendChild(img);
+});
