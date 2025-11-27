17a366cc45d34a8844afd7608848c36816cd6431
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2025-11-27 09:16:52 +0100

    Added the function to move to slide
    Buttons are now created with js, one for each image.
    Both images and buttons share the same
    id number (img-x, btn-x).
    Added the function to move to a particular slide by
    clicking on the "skip" buttons. If clicking the same
    does nothing.
    Need to make sure the order of the images stays the same
    otherwise, when mixing next/previous buttons and skip
    buttons clicks we don't want users to go looking for the
    image they saw but can't find.
    Need to color the button selected so that we know at what
    image we are.
    Need to make the next/previous functions.

--- src/index.js
+++ src/index.js
@@ -11,11 +11,56 @@ const images = importAll(
   require.context('../assets', false, /\.(png|jpe?g|svg)$/)
 );
 
-// 3. Append to DOM
+// 3. Append images and respective buttons to DOM
 const container = document.querySelector('.container');
+const img_window = document.querySelector('.image-window')
+let img_counter = 0; //used to give unique ids to images
+
+const btn_slide = document.querySelector(".btn-slide");
 
 images.forEach(src => {
-  const img = document.createElement('img');
-  img.src = src; // Webpack replaces this with the final file URL
-  container.appendChild(img);
+  setUpImg(src);
+  setUpBtn();
+  img_counter++;
 });
+
+function setUpBtn() {
+  const btn = document.createElement('button');
+  btn.setAttribute("id", `btn-${img_counter}`);
+  btn.addEventListener("click", (e) => changeSlide(e));
+  btn_slide.appendChild(btn);
+};
+
+function setUpImg(src) {
+  const img = document.createElement('img');
+  
+  img.src = src; // Webpack replaces this with the final file URL
+  img.setAttribute("id", `img-${img_counter}`);
+  
+  if (img_counter === 0) {
+    img_window.appendChild(img);
+  } else {
+    container.appendChild(img);
+  }
+};
+
+function changeSlide(e) {
+  //Get the ids of the current image and the clicked button
+  const currImg = img_window.children[0];
+  const id_img_current = currImg.getAttribute("id").split("-")[1];
+  const id_btn_clicked = e.srcElement.id.split("-")[1];
+
+  if (id_img_current === id_btn_clicked) {
+    return
+  } else {
+    //Swap the images
+    container.appendChild(currImg);
+    img_window.appendChild(document.getElementById(`img-${id_btn_clicked}`));
+  }
+};
+
+
+
+
+
+
