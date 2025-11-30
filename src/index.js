8aa54f2ec4b86a87edc51059f64c32eab432003a
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2025-11-30 20:04:11 +0100

    Added prettierrc
    Basic changes to the code, nothing substancial

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import './style.css';
+import "./style.css";
 
 //Factory function to import images
 export function loadImages() {
   // 1. The function r (require.context(...)) gives r.keys() which are the images paths
   // and r(key) which gives the built asset URL
   function importAll(r) {
-    return r.keys().map(r); 
+    return r.keys().map(r);
   }
 
   // 2. Create a require.context instance - a webpack function
   const images = importAll(
-    require.context('../assets', false, /\.(png|jpe?g|svg)$/)
+    require.context("../assets", false, /\.(png|jpe?g|svg)$/),
   );
 
+  //need to return images othwerise nothing is returned and the importAll return (saved in images) is lost
   return images;
 }
 
@@ -22,9 +23,8 @@ export function imageCarousel({
   viewportSelector,
   btnContainerSelector,
   nextBtnSelector,
-  prevBtnSelector
+  prevBtnSelector,
 }) {
-
   // DOM references
   const container = document.querySelector(containerSelector);
   const img_window = document.querySelector(viewportSelector);
@@ -48,74 +48,76 @@ export function imageCarousel({
 
   function setUpImg() {
     images.forEach((src, index) => {
-      const img = document.createElement('img');
-    
+      const img = document.createElement("img");
+
       img.src = src; // Webpack replaces this with the final file URL
       img.setAttribute("id", `img-${index}`);
-      
+
       if (index === 0) {
         img_window.appendChild(img);
-
       } else {
         container.appendChild(img);
       }
 
+      //append the reference to the DOM node in the array
       imgElements.push(img);
     });
   }
 
   function setUpBtn() {
     images.forEach((_, index) => {
-      const btn = document.createElement('button');
+      const btn = document.createElement("button");
       btn.setAttribute("id", `btn-${index}`);
       if (index === 0) {
         btn.classList.add("active");
       }
       btn.addEventListener("click", () => showImage(index));
       btn_slide.appendChild(btn);
-      
+
+      //append the reference to the DOM node in the array
       btnElements.push(btn);
     });
   }
 
   function showImage(index) {
-    console.log(index, currentIndex);
     if (index === currentIndex) return;
 
     //Otherwise
     container.appendChild(imgElements[currentIndex]);
-    const newImg = document.getElementById(`img-${index}`);
+    const newImg = imgElements[index];
     img_window.appendChild(newImg);
 
     //Toggle the active class to show which slide is selected
-    const currBtn = document.getElementById(`btn-${currentIndex}`);
-    const newBtn = document.getElementById(`btn-${index}`);
+    //This works because in btnElements are stored references to the
+    //DOM nodes and not copies!!
+    const currBtn = btnElements[currentIndex];
+    const newBtn = btnElements[index];
     currBtn.classList.toggle("active");
     newBtn.classList.toggle("active");
 
     currentIndex = index;
-    console.log("current index after:", currentIndex);
-  };
+  }
 
   function goNext() {
     const nextIndex = (currentIndex + 1) % imgElements.length;
     showImage(nextIndex);
-  };
+  }
 
   function goPrev() {
-    const prevIndex = (currentIndex - 1 + imgElements.length) % imgElements.length;
+    const prevIndex =
+      (currentIndex - 1 + imgElements.length) % imgElements.length;
     showImage(prevIndex);
-  };
+  }
 
   function attachEvents() {
     nextBtn.addEventListener("click", goNext);
     prevBtn.addEventListener("click", goPrev);
   }
-  
+
   function startAutoPlay(interval = 5000) {
-    return timer = setInterval(goNext,interval);
+    return (timer = setInterval(goNext, interval));
   }
-  
+
   function stopAutoPlay() {
     clearInterval(timer);
   }
@@ -125,6 +127,6 @@ export function imageCarousel({
     goNext,
     goPrev,
     startAutoPlay,
-    stopAutoPlay
+    stopAutoPlay,
   };
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { loadImages, imageCarousel } from "./index.js";
 
 (() => {
-    const images = loadImages();
+  const images = loadImages();
 
-    const carousel = imageCarousel({
-        images,
-        containerSelector: '.container',
-        viewportSelector: '.image-window',
-        btnContainerSelector: ".btn-slide",
-        nextBtnSelector: "#fwd-btn",
-        prevBtnSelector: '#back-btn'
-    });
+  const carousel = imageCarousel({
+    images,
+    containerSelector: ".container",
+    viewportSelector: ".image-window",
+    btnContainerSelector: ".btn-slide",
+    nextBtnSelector: "#fwd-btn",
+    prevBtnSelector: "#back-btn",
+  });
 
-    carousel.init();
+  carousel.init();
 })();
